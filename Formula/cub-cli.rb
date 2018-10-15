918dbca7ac2327afaa07cafa5fb69108336c8383
Andrew Huynh <a5thuynh@gmail.com>
2018-10-15 00:48:34 -0700

    Bump to version 0.2.0

--- Formula/cub-cli.rb
+++ Formula/cub-cli.rb
@@ -1,8 +1,8 @@
 class CubCli < Formula
   desc "A CLI for the Bear Writer application"
   homepage "https://github.com/a5huynh/cub-cli"
-  url "https://github.com/a5huynh/cub-cli/releases/download/0.1.0/cub"
-  sha256 "c65caf3435b5906457f6e12ff6e5f3c97baf6e828e656c222db38daaf8cac7e4"
+  url "https://github.com/a5huynh/cub-cli/releases/download/0.2.0/cub"
+  sha256 "9b395f58c9787e7c2309c511bb14970738c994ce9675d4089be042d81e8850cd"
 
   def install
     bin.install "cub"
