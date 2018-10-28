1c26fb519be0e3c0970d590be93ad314683f44a9
Andrew Huynh <a5thuynh@gmail.com>
2018-10-28 10:02:22 -0700

    Updating SHA256 for `cub-cli`

--- Formula/cub-cli.rb
+++ Formula/cub-cli.rb
@@ -2,7 +2,7 @@ class CubCli < Formula
   desc "A CLI for the Bear Writer application"
   homepage "https://github.com/a5huynh/cub-cli"
   url "https://github.com/a5huynh/cub-cli/releases/download/0.3.0/cub"
-  sha256 "36b374dc67f8ec6dcae2e967543b55df924b31216699b3c0aee2deee97e3a647"
+  sha256 "f9987fbd0002324835ee721d8a11550df53f0e31ab0d668b7aa1ac39303fcf2c"
 
   def install
     bin.install "cub"
